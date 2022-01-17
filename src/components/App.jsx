e3b0d69251000954128ab3186e7c5a55dbf956d0
Anna Samsonenko <foster.anna93@gmail.com>
2022-01-17 10:48:02 +0200

    set contacts to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,56 @@
-import { Component } from "react";
-import { GlobalStyle } from "common/GlobalStyle";
-import { ContactForm } from "./ContactForm/ContactForm";
-import { ContactList } from "./ContactList/ContactList";
-import { Filter } from "./Filter/Filter";
-import { Container, Title, SubTitle } from "./AppStyled";
+import { Component } from 'react';
+import { GlobalStyle } from 'common/GlobalStyle';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Container, Title, SubTitle } from './AppStyled';
 export class App extends Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    filter: "",
+    filter: '',
   };
 
-  onSubmitHandler = (contactObj) => {
+  storageKey = 'contacts';
+
+  componentDidMount() {
+    const storageContacts = localStorage.getItem(this.storageKey);
+    const parsedStorageContacts = JSON.parse(storageContacts);
+
+    if (parsedStorageContacts) {
+      this.setState({ contacts: parsedStorageContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(this.storageKey, JSON.stringify(this.state.contacts));
+    }
+  }
+
+  onSubmitHandler = contactObj => {
     this.setState(({ contacts }) => {
-      return { contacts: [contactObj, ...contacts], filter: "" };
+      return { contacts: [contactObj, ...contacts], filter: '' };
     });
   };
 
-  onChangeHandler = (filter) => {
-    this.setState((prevState) => {
+  onChangeHandler = filter => {
+    this.setState(prevState => {
       return { ...prevState, filter: filter };
     });
   };
 
-  onDeleteHandler = (id) => {
-    const filtredContacts = this.state.contacts.filter(
-      (contact) => contact.id !== id
-    );
-    this.setState((prevState) => {
+  onDeleteHandler = id => {
+    const filtredContacts = this.state.contacts.filter(contact => contact.id !== id);
+    this.setState(prevState => {
       return { ...prevState, contacts: [...filtredContacts] };
     });
   };
+
   render() {
     const { contacts, filter } = this.state;
     return (
@@ -46,11 +62,7 @@ export class App extends Component {
 
         <SubTitle>Contacts</SubTitle>
         <Filter onChange={this.onChangeHandler} />
-        <ContactList
-          contacts={contacts}
-          filter={filter}
-          onDelete={this.onDeleteHandler}
-        />
+        <ContactList contacts={contacts} filter={filter} onDelete={this.onDeleteHandler} />
       </Container>
     );
   }
